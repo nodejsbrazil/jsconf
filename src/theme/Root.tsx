d20b16973c9efaf175db0a5f2bdd55d79120e47f
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-01-26 07:12:06 -0300

    chore: remove unused context

--- src/theme/Root.tsx
+++
@@ -1,5 +0,0 @@
-import { RootProvider } from '@site/src/components/contexts/Root';
-
-export default function Root({ children }: { children: React.ReactNode }) {
-  return <RootProvider>{children}</RootProvider>;
-}
