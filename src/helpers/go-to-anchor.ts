a9eab10ca80eb6563d40c894d50e48163f0d0087
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-01-26 22:18:44 -0300

    fix: apply team requests I

--- src/helpers/go-to-anchor.ts
+++ src/helpers/go-to-anchor.ts
@@ -1,17 +1,19 @@
-export const goToAnchor = (id: string) => {
-  const body = document.querySelector<HTMLElement>('#__docusaurus');
-  if (!body) return;
+export const goToAnchor = (id: string): void => {
+  try {
+    const body = document.querySelector<HTMLElement>('#__docusaurus');
+    if (!body) return;
 
-  const anchor = document.querySelector<HTMLElement>(id);
-  if (!anchor) return;
+    const anchor = document.querySelector<HTMLElement>(id);
+    if (!anchor) return;
 
-  const top = body
-    ? anchor.offsetTop - body.offsetTop
-    : anchor.getBoundingClientRect().top + window.scrollY;
+    const top = body
+      ? anchor.offsetTop - body.offsetTop
+      : anchor.getBoundingClientRect().top + window.scrollY;
 
-  (body || window).scrollTo({
-    top,
-    left: 0,
-    behavior: 'smooth',
-  });
+    (body || window).scrollTo({
+      top,
+      left: 0,
+      behavior: 'smooth',
+    });
+  } catch {}
 };
