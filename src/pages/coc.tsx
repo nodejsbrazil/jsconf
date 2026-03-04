98cb0f2c64a06bb701bff4011adb493747dc15cc
lojhan <viniciuslojhan@gmail.com>
2026-03-04 10:11:32 -0300

    feat: add Spanish CoC

--- src/pages/coc.tsx
+++ src/pages/coc.tsx
@@ -2,14 +2,28 @@ import '@site/src/scss/pages/markdown.scss';
 import { translate } from '@docusaurus/Translate';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import CoC_enUS from '../assets/md/CoC.en-US.md';
+import CoC_es419 from '../assets/md/CoC.es-419.md';
 import CoC_ptBR from '../assets/md/CoC.md';
 import { Page } from '../components/shared/Page';
+
+const selectCoC = (locale: string) => {
+  switch (locale) {
+    case 'en-US':
+      return CoC_enUS;
+    case 'es-419':
+      return CoC_es419;
+    case 'pt-BR':
+      return CoC_ptBR;
+    default:
+      return CoC_ptBR;
+  }
+};
 
 export default () => {
   const {
     i18n: { currentLocale },
   } = useDocusaurusContext();
-  const CoC = currentLocale === 'en-US' ? CoC_enUS : CoC_ptBR;
+  const CoC = selectCoC(currentLocale);
 
   return (
     <Page
