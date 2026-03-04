9abdf227df9440da21ef00391ef1b27df8fd3aef
ananeridev <ananeri32@hotmail.com>
2026-03-03 22:00:41 -0300

    ticket with sympla
    Signed-off-by: ananeridev <ananeri32@hotmail.com>

--- src/configs/definitions.ts
+++ src/configs/definitions.ts
@@ -1,4 +1,5 @@
 export const link = {
-  tickets: 'https://www.sympla.com.br/',
+  tickets:
+    'https://www.sympla.com.br/preview/70b7073f5188ef1c79a98890c746ac38?_gl=1*1rg1w4*_gcl_au*MTIzMTI4MTc0Ni4xNzcyNTg1MzQwLjI1NTM4NDg5MS4xNzcyNTg1OTQzLjE3NzI1ODU5NDM.*_ga*NjUyODY1MTU0LjE3NzI1ODUzNDA.*_ga_KXH10SQTZF*czE3NzI1ODUzNDAkbzEkZzEkdDE3NzI1ODU5ODkkajQwJGwwJGg3NDMwMzIxNDE.',
   sponsors: 'https://forms.gle/SPyyD3SsuurVpvCNA',
 } as const;
