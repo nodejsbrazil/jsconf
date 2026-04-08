c1f0b4982ecb36d241e6b8e196238e19225596a6
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-04-08 19:12:12 -0300

    chore: update C4P end

--- src/website/pages/c4p/_components/introduction.tsx
+++ src/website/pages/c4p/_components/introduction.tsx
@@ -30,7 +30,7 @@ export const Introduction = () => {
         </h2>
         <p className={styles.paragraph}>
           Aceitaremos inscrições até{' '}
-          <span className='text-primary !font-bold'>**/**</span>.
+          <span className='text-primary !font-bold'>31/08</span>.
         </p>
         <p className={styles.paragraph}>
           Você pode enviar até{' '}
